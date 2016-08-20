db86d8e71399361cd605369b5727da8875734f17
Pan Yingxue <panyingxue@youku.com>
2016-08-21 02:00:23 +0800

    modulehelperv1.0

--- modulehelper.sh
+++ modulehelper.sh
@@ -13,6 +13,17 @@ function capitalize() {
     echo "${split[*]^}"
 }
 
+function substr() {
+    local str1=$1
+    local str2=$2
+
+    if [[ ${str1/${str2}//} == $str1 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 function checkParams() {
     case "$1" in
         "create")
@@ -95,13 +106,49 @@ case "$operation" in
         sed -i "s/$templatename/$modulename/g" $moduleController
         sed -i "s/$templatenameCap/$modulenameCap/g" $moduleController
 
+        index=0
+        sqlflag=0
+        createsqlitems=""
+        selectsqlitems=""
+        insertsqlcolumn=""
+        insertsqlvalue=""
+        updatesqlitems=""
+        jsonitems="    "
         templateSrvData=$templatesdatapath/${templatename}SrvData.txt
+        while IFS='|' read -ra sqlcolumn; do
+            if [ $index -ne 0 ]; then
+                createsqlitems=${createsqlitems},
+                selectsqlitems=${selectsqlitems},
+                jsonitems="${jsonitems}\n    "
+            fi
+            for info in "${sqlcolumn[@]}"; do
+                createsqlitems="${createsqlitems} ${info}"
+            done
+            if [ $sqlflag -ne 0 ]; then
+                insertsqlcolumn=${insertsqlcolumn},
+                insertsqlvalue=${insertsqlvalue},
+                updatesqlitems=${updatesqlitems},
+            fi
+            selectsqlitems="${selectsqlitems} ${sqlcolumn[0]}"
+            if [ ${sqlcolumn[0]} != "id" ]; then
+                insertsqlcolumn="${insertsqlcolumn} ${sqlcolumn[0]}"
+                insertsqlvalue="${insertsqlvalue} ?"
+                updatesqlitems="${updatesqlitems} ${sqlcolumn[0]}=?"
+                sqlflag=1
+            fi
+            columnNameCap=`capitalize ${sqlcolumn[0]}`
+            substr ${sqlcolumn[1]} "char"
+            if [ $? -eq 1 ]; then
+                jsonitems="${jsonitems} ${columnNameCap}  string \`json:\"${sqlcolumn[0]}\"\`"
+            else
+                jsonitems="${jsonitems} ${columnNameCap} int64 \`json:\"${sqlcolumn[0]}\"\`"
+            fi
+            index=$(( $index + 1 ))
+        done < $templateSrvData
+        
+        createsql="create table ${modulename} ($createsqlitems)"
         moduleSrvData=$datapath/${modulename}SrvData.sql
         echo "Cteate $moduleSrvData ... "
-        #touch $moduleSrvData
-        totalline=`awk -F"|" 'END {print(NR)}' $templateSrvData`
-        sqlitems=`awk -F"|" '{if(NR != "'$totalline'") printf("%s %s %s,", $1, $2, $3); else printf("%s %s %s", $1, $2, $3);}' $templateSrvData`
-        createsql="create table ${modulename} ($sqlitems)"
         echo $createsql > $moduleSrvData
 
         moduleSrvModel=$modelpath/${modulename}SrvModel.go
@@ -109,11 +156,17 @@ case "$operation" in
         cp $templatesmodelpath/${templatename}SrvModel.go $moduleSrvModel
         sed -i "s/$templatename/$modulename/g" $moduleSrvModel
         sed -i "s/$templatenameCap/$modulenameCap/g" $moduleSrvModel
-        items=`awk -F"|" '{if($2 ~ "char") printf("%s %s \\\\\`json:\"%s\"\\\\\`\\n", $1, "string", $1); else printf("%s %s \\\\\`json:\"%s\"\\\\\`\\n", $1, "int64", $1);}' $templateSrvData`
-        ITEM="##Item##"
-        echo $ITEM
-        echo $items
-        sed -i "s/${ITEM}/```${items}```/g" $moduleSrvModel
+        sed -i "s/##Item##/${jsonitems}/g" $moduleSrvModel
+        selectsql="select${selectsqlitems} from ${modulename}"
+        selectonesql="select${selectsqlitems} from ${modulename} where id=?"
+        insertsql="insert into ${modulename}(${insertsqlcolumn}) values(${insertsqlvalue})"
+        updatesql="update ${modulename} set${updatesqlitems} where id=?"
+        deletesql="delete from ${modulename} where id=?"
+        sed -i "s/##query##/${selectsql}/g" $moduleSrvModel
+        sed -i "s/##queryone##/${selectonesql}/g" $moduleSrvModel
+        sed -i "s/##insert##/${insertsql}/g" $moduleSrvModel
+        sed -i "s/##update##/${updatesql}/g" $moduleSrvModel
+        sed -i "s/##delete##/${deletesql}/g" $moduleSrvModel
         ;;
     "remove")
         echo "Remove the existed module $modulename"
